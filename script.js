53cafd8db62f89d54c599f268b49ceaa56a9411d
Aditya Tuljapurkar <139285414+AdityaTuljapurkar@users.noreply.github.com>
2023-11-10 14:50:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+function togglehide1() {
+  let btn = document.getElementById("btn1");
+  let para = document.getElementById("quespara1");
+
+    
+  if (para.style.display != "none") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
+}
+
+function togglehide2() {
+  let btn = document.getElementById("btn2");
+  let para = document.getElementById("quespara2");
+
+  
+  if (para.style.display != "none") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
+}
+function togglehide3() {
+  let btn = document.getElementById("btn3");
+  let para = document.getElementById("quespara3");
+
+  if (para.style.display != "none") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
+}
+function togglehide4() {
+  let btn = document.getElementById("btn4");
+  let para = document.getElementById("quespara4");
+
+  if (para.style.display != "none") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
+}
+function togglehide5() {
+  let btn = document.getElementById("btn5");
+  let para = document.getElementById("quespara5");
+
+  if (para.style.display != "none") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
+}
+function togglehide6() {
+  let btn = document.getElementById("btn6");
+  let para = document.getElementById("quespara6");
+
+  if (para.style.display != "none") {
+    para.style.display = "none";
+  } else {
+    para.style.display = "block";
+  }
+}
